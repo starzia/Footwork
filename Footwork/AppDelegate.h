859a4c60bbf0b62904eec9f484b980212e7ebf20
Steve Tarzia <steve@stevetarzia.com>
2012-05-13 20:24:27 -0500

    Initial Commit

---
+++ Footwork/AppDelegate.h
@@ -0,0 +1,19 @@
+//
+//  AppDelegate.h
+//  Footwork
+//
+//  Created by Stephen Tarzia on 5/13/12.
+//  Copyright (c) 2012 VaporStream, Inc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@class ViewController;
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (strong, nonatomic) ViewController *viewController;
+
+@end
---
+++ Footwork/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  Footwork
+//
+//  Created by Stephen Tarzia on 5/13/12.
+//  Copyright (c) 2012 VaporStream, Inc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
